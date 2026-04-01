42d4e58faaf13158ca870dd4f2300beb99ab588e
edubew <edubew98@gmail.com>
2026-04-01 06:51:41 +0300

    Prepare frontend for deployment

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -24,9 +24,6 @@ function Login() {
     try {
       const loggedInUser = await login(form);
       console.log("Logged in user:", loggedInUser);
-      // if (loggedInUser) {
-      //   navigate("/dashboard", { replace: true });
-      // }
       navigate("/dashboard")
     } catch (err) {
       setError(err.response?.data?.error || "Invalid email or password");
